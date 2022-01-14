4b15affe8cacf65e24804273240bdc2555dda553
M0rz4n <m0rz4n@protonmail.com>
2022-01-14 19:33:30 +0100

    añadido Notas.py

---
+++ notas.py
@@ -0,0 +1,11 @@
+#No tengo claro como abrir los programas a traves de python, por lo que valoro hacer un notepad con tkinder
+
+def main():
+    print("Escriba y presione ENTER para guardar")
+    with open("Notas.txt") as note:
+        note.write(input())
+    print("Nota guardada")
+
+
+if __name__ == "__main__":
+    main()
